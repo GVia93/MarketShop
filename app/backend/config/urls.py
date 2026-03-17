f4e6a422a879625665c6185f160565b353b74d83
viach <gritcaev.vv@gmail.com>
2026-03-17 16:58:32 +0300

    fix

--- app/backend/config/urls.py
+++ app/backend/config/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+else:
+    # В продакшене статику раздаёт nginx, но медиа всё равно нужно
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
